084dfcff467f2c1d36771bf4a109f1af1e512fa9
nsweb <nsweb@nsweb.fr>
2016-05-09 13:22:32 +0200

    adding a bunch of intersection functions

--- engine/src/math/intersections.cpp
+++ engine/src/math/intersections.cpp
@@ -8,6 +8,7 @@ namespace bigball
 //////////////////////////////////////////////////////////////////////////
 // Explicit instantiation
 template struct Intersect<double>; 
+template struct Intersect<float>;
 
 /* Fast Ray-Box Intersection by Andrew Woo */
 template <typename T>
@@ -91,6 +92,84 @@ bool Intersect<T>::RayAABB( Vec3<T> const& min_box, Vec3<T> const& max_box, Vec3
 	return true;		/* ray hits box */
 }
 
+template <typename T>
+T Intersect<T>::SquaredDistancePointSegment(Vec3<T> const& point, Vec3<T> const& seg0, Vec3<T> const& seg1, T& t_on_segment)
+{
+	Vec3<T> V_seg0 = point - seg0;
+	Vec3<T> dir_seg = seg1 - seg0;
+	T t = bigball::max(dot(dir_seg, V_seg0), (T)0.0);
+
+	T dot_dd = dot(dir_seg, dir_seg);
+	t = min(t, dot_dd) / dot_dd;
+	V_seg0 += dir_seg*(-t);
+
+	t_on_segment = t;
+	return dot(V_seg0, V_seg0);
+}
+
+template <typename T>
+T Intersect<T>::RaySphereIntersection(Vec3<T> const& ray_orig, Vec3<T> const& ray_dir, Vec3<T> const& center, T radius)
+{
+	// get the offset vector
+	Vec3<T> offset = center - ray_orig;
+
+	// get the distance along the ray to the center point of the sphere
+	T ray_dist = dot(ray_dir, offset);
+	if (ray_dist <= T(0.))
+	{
+		// moving away from object
+		return T(-1.);
+	}
+
+	// get the squared distances
+	T off_2 = dot(offset, offset);
+	T rad_2 = radius * radius;
+	if (off_2 <= rad_2)
+	{
+		// we're in the sphere
+		return T(0.);
+	}
+
+	// find hit distance squared
+	T d_2 = rad_2 - (off_2 - ray_dist * ray_dist);
+	if (d_2 < T(0.))
+	{
+		// ray passes by sphere without hitting
+		return T(-1.);
+	}
+
+	// get the distance along the ray
+	T dist = ray_dist - sqrt(d_2);
+
+	return dist;
+}
+
+template <typename T>
+T Intersect<T>::RayBoxIntersection(Vec3<T> const& ray_orig, Vec3<T> const& ray_dir, Vec3<T> const& box_ratio)
+{
+	T tx1 = (-box_ratio.x - ray_orig.x) / ray_dir.x;
+	T tx2 = (box_ratio.x - ray_orig.x) / ray_dir.x;
+
+	T tmin = min(tx1, tx2);
+	T tmax = max(tx1, tx2);
+
+	T tz1 = (-box_ratio.z - ray_orig.z) / ray_dir.z;
+	T tz2 = (box_ratio.z - ray_orig.z) / ray_dir.z;
+
+	tmin = max(tmin, min(tz1, tz2));
+	tmax = min(tmax, max(tz1, tz2));
+
+	T ty1 = (-box_ratio.y - ray_orig.y) / ray_dir.y;
+	T ty2 = (box_ratio.y - ray_orig.y) / ray_dir.y;
+
+	tmin = max(tmin, min(ty1, ty2));
+	tmax = min(tmax, max(ty1, ty2));
+
+	if (tmax < tmin)
+		return T(-1.);
+
+	return tmin;
+}
 
 } /* namespace bigball */
 
--- engine/src/math/intersections.h
+++ engine/src/math/intersections.h
@@ -14,20 +14,9 @@ typedef Intersect<double> dintersect;
 template <typename T> struct Intersect
 {
 	static bool RayAABB( Vec3<T> const& minB, Vec3<T> const& maxB, Vec3<T> const& origin, Vec3<T> const& dir, Vec3<T>& coord );
-  
-    static T SquaredDistancePointSegment( Vec3<T> const& point, Vec3<T> const& seg0, Vec3<T> const& seg1, T& t_on_segment )
-    {
-        Vec3<T> V_seg0  = point - seg0;
-        Vec3<T> dir_seg = seg1 - seg0;
-        T t             = bigball::max( dot( dir_seg, V_seg0 ), (T)0.0 );
-        
-        T dot_dd        = dot( dir_seg, dir_seg );
-        t               = min( t, dot_dd ) / dot_dd;
-        V_seg0          += dir_seg*(-t);
-        
-        t_on_segment    = t;
-        return dot( V_seg0, V_seg0 );
-    }
+	static T SquaredDistancePointSegment(Vec3<T> const& point, Vec3<T> const& seg0, Vec3<T> const& seg1, T& t_on_segment);
+	static T RaySphereIntersection(Vec3<T> const& ray_orig, Vec3<T> const& ray_dir, Vec3<T> const& center, T radius);
+	static T RayBoxIntersection(Vec3<T> const& ray_orig, Vec3<T> const& ray_dir, Vec3<T> const& box_ratio);
 };
 
 } /* namespace bigball */
